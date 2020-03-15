04c2afdc21e843c34030ee708a52375dedbb6980
Gergely Módi <gmodi@chemaxon.com>
2020-03-15 15:52:20 +0100

    GeReact dynamic render

--- src/poor-mans-react/index.tsx
+++ src/poor-mans-react/index.tsx
@@ -1,17 +1,29 @@
+import { Observable, BehaviorSubject, isObservable, Subject } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+
 const React = {
   createElement: (tag, props, ...children) => {
     if (typeof tag === 'function') return tag(props);
-    console.log({ tag, props: { ...props, children } });
     return { tag, props: { ...props, children } };
   }
 };
-const useState = initialState => {};
 
 const render = (reactElement, container: Node) => {
   if (['string', 'number'].includes(typeof reactElement)) {
-    container.appendChild(document.createTextNode(reactElement));
+    const newTextElement = document.createTextNode(reactElement);
+
+    container.appendChild(newTextElement);
+    return newTextElement;
+  }
+
+  if (isObservable(reactElement)) {
+    const subject = new Subject();
+    subject.subscribe(console.log);
+    reactElement.subscribe(element => subject.next(render(element, container)));
     return;
   }
+
+  if (!reactElement) return;
 
   const newDOMelement = document.createElement(reactElement.tag);
   if (reactElement.props) {
@@ -23,16 +35,44 @@ const render = (reactElement, container: Node) => {
     reactElement.props.children.forEach(child => render(child, newDOMelement));
   }
   container.appendChild(newDOMelement);
+  return newDOMelement;
 };
 
-const App = () => (
-  <div>
-    <h1>Na szevasz</h1>
-    <div>Valami josag</div>
-    <div className="input">
-      <input onChange={e => console.log('buzika')} />
+function useRxState<State>(initialState: State): [Observable<State>, (callbackFn: (state: State) => State) => void] {
+  const distributor = new BehaviorSubject<State>(initialState);
+  const updater = (callbackFn: (state: State) => State) =>
+    distributor.pipe(first()).subscribe(state => distributor.next(callbackFn(state)));
+  return [distributor, updater];
+}
+
+const App = () => {
+  const [state, updateState] = useRxState(0);
+  return (
+    <div>
+      <h1>Na szevasz</h1>
+      <div>Valami josag</div>
+      <div>
+        {state}
+        <button onClick={() => updateState(state => state + 1)}>+1</button>
+      </div>
+      {state.pipe(
+        map(
+          state =>
+            state > 5 && (
+              <div>
+                Its bigger than 5 man!
+                <div>
+                  <img src="https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif" />
+                </div>
+              </div>
+            )
+        )
+      )}
+      <div className="input">
+        <input onChange={e => console.log('buzika')} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 render(<App />, document.querySelector('#root'));
